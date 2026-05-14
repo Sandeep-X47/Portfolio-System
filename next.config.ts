a56e3250d7d55b8a540cdb1aa245c4059eccde26
Sandeep Kumar S <snxd2047@gmail.com>
2026-05-14 14:03:47 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
+  output: "export", // 🔥 this is the key for GitHub Pages
+  images: {
+    unoptimized: true // required for static export
+  }
 };
 
 export default nextConfig;

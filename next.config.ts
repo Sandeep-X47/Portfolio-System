3865a01c4299d98682104990db76c2092593e7cc
Sandeep Kumar <168741924+Sandeep-X47@users.noreply.github.com>
2026-05-14 14:22:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === "production"
-    ? "/Portfolio-System"
-    : ""
+  devIndicators: false,
 };
 
 export default nextConfig;
